ce2a1babeaf90f7eb0b728a2a4dd86454bf66c25
Shashikant Yadav <yshashikant231217@gmail.com>
2026-01-12 10:59:46 +0530

    Update chatWindow.jsx

--- frontend/sigmaGPT/src/chatWindow.jsx
+++ frontend/sigmaGPT/src/chatWindow.jsx
@@ -26,7 +26,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("http://localhost:8080/api/chat", options);
+            const response = await fetch("https://sigmagpt-5g8w.onrender.com/api/chat", options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply);
